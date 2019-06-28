fa4db7fb8ccdb84e3c13b5da6f991057e914326c
Ivanildo Junior <ivanildo@outlook.com>
2019-06-28 23:01:38 +0000

    Criadas novas activitys

---
+++ app/src/main/java/com/ufersacc/bitniquel/InformacoesActivity.java
@@ -0,0 +1,13 @@
+package com.ufersacc.bitniquel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InformacoesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_principal);
+    }
+}
--- app/src/main/java/com/ufersacc/bitniquel/MainActivity.java
+++ app/src/main/java/com/ufersacc/bitniquel/MainActivity.java
@@ -2,6 +2,8 @@ package com.ufersacc.bitniquel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void telaPrincipal(View view){
+        Intent intent = new Intent(this, PrincipalActivity.class);
+        startActivity(intent);
+    }
+    
 }
