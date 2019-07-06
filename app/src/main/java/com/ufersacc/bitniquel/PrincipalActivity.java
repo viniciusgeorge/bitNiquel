f10075d213f0ef6c682a06d13c2191cdd2b22afb
viniciusgeorge <viniciusveorge@gmail.com>
2019-07-05 21:25:59 -0300

    QRcode

--- app/src/main/java/com/ufersacc/bitniquel/PrincipalActivity.java
+++ app/src/main/java/com/ufersacc/bitniquel/PrincipalActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.barcode.Barcode;
+import com.notbytes.barcode_reader.BarcodeReaderActivity;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -27,8 +31,24 @@ public class PrincipalActivity extends AppCompatActivity {
 
     void telaPagamento(View view)
     {
-        Intent intent = new Intent(PrincipalActivity.this, PagamentoActivity.class);
-        startActivity(intent);
+        Intent launchIntent = BarcodeReaderActivity.getLaunchIntent(this, true, false);
+        startActivityForResult(launchIntent, 1208);
+
+
+        //Intent intent = new Intent(PrincipalActivity.this, PagamentoActivity.class);
+        //startActivity(intent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != PrincipalActivity.RESULT_OK) {
+            Toast.makeText(this, "error in  scanning", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (requestCode == 1208 && data != null) {
+            Barcode barcode = data.getParcelableExtra(BarcodeReaderActivity.KEY_CAPTURED_BARCODE);
+            Toast.makeText(this, barcode.rawValue, Toast.LENGTH_SHORT).show();
+        }
     }
 
     void telaExtrato(View view)
